7750877032a9de6fde55b1621e035a1ade472e4c
Emre Ayaz <ayazthemre@gmail.com>
2023-05-25 13:28:24 +0300

    tasks endpoints update

--- example/clickup_dart_example.dart
+++ example/clickup_dart_example.dart
@@ -6,6 +6,7 @@ void main() async {
   final clickUp = ClickUp(apiEndpoint: "https://a00fb6e0-339c-4201-972f-503b9932d17a.remockly.com")..initialize(authToken: token);
 
   final attachment = await clickUp.attachments.createTaskAttachment(taskID: "8669e046h", useCustomTaskID: false, filePath: "./test/beksinski_sample.jpg");
-
   print(attachment);
+  final listComments = await clickUp.comments.getListComments(listID: 1);
+  print(listComments);
 }
